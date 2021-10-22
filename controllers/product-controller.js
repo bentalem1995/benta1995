6c937b601a701f82dc25bf9d4d33985117cbc8d0
bentalem14 <92831434+bentalem14@users.noreply.github.com>
2021-10-22 22:05:14 +0300

    adding images for second time

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -31,7 +31,7 @@ const fileFilter =(req,file,cb)=>{
 const upload = multer({storage:storage,fileFilter:fileFilter,limits:{fileSize:1024*1024*5}})
 router.get("/images/:image", (req, res) => {
     try {
-        const image = req.params.image;
+        const image = +req.params.image;
         return (res.sendFile(__dirname + "/assets/image/" + image));
     } catch (err) {
         response.status(400).send(errorHandler(err));
