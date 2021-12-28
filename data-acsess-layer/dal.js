277a98e010ee541701cc9d3db58b8ee8ebc86cdd
bentalem1995 <92836616+bentalem1995@users.noreply.github.com>
2021-12-28 17:59:52 +0200

    Update dal.js

--- data-acsess-layer/dal.js
+++ data-acsess-layer/dal.js
@@ -3,8 +3,9 @@ const connection = mysql.createConnection(
     {
         host:process.env.SQL_HOST,
         database:process.env.SQL_DATABASE,
-        user:process.env.SQL.USER,
-        password:process.env.SQL.PASSWORD,
+        user:process.env.SQL_USER,
+        password:process.env.SQL_PASSWORD,
+        port:3306
         
 
     });
